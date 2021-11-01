21acd6319f19368606d7ac9246a2219d508221d5
jmmunoza <jmmunoza@eafit.edu.co>
2021-11-01 12:49:13 -0500

    mas mejoras de la conexion a mongo

--- app/src/main/java/com/globapp/globapp/view/adapters/NewsListAdapter.java
+++ app/src/main/java/com/globapp/globapp/view/adapters/NewsListAdapter.java
@@ -1,18 +1,11 @@
 package com.globapp.globapp.view.adapters;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
-import android.view.GestureDetector;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.globapp.globapp.R;
@@ -21,19 +14,14 @@ import com.globapp.globapp.data.listeners.OnNewsLikedListener;
 import com.globapp.globapp.data.listeners.OnUserImageClickedListener;
 import com.globapp.globapp.data.local.Preferences;
 import com.globapp.globapp.model.News;
-import com.globapp.globapp.util.SingleTapConfirm;
 import com.globapp.globapp.util.UserNameGetter;
-import com.globapp.globapp.view.MainActivity;
-import com.globapp.globapp.view.dialogs.CommentDialog;
+import com.globapp.globapp.view.viewholders.NewsListViewHolder;
 
 import org.bson.types.ObjectId;
 
 import java.util.ArrayList;
 
-import pl.droidsonroids.gif.GifDrawable;
-import pl.droidsonroids.gif.GifImageView;
-
-public class NewsListAdapter extends RecyclerView.Adapter<NewsListAdapter.ViewHolder> {
+public class NewsListAdapter extends RecyclerView.Adapter<NewsListViewHolder> {
 
     private final ArrayList<News>    newsList;
     private final LayoutInflater     inflater;
@@ -52,19 +40,27 @@ public class NewsListAdapter extends RecyclerView.Adapter<NewsListAdapter.ViewHo
     }
 
     @NonNull @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public NewsListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         if(Preferences.getDarkMode()){
-            return new ViewHolder(inflater.inflate(R.layout.fragment_news_item_dark, parent, false));
+            return new NewsListViewHolder(
+                    inflater.inflate(R.layout.fragment_news_item_dark, parent, false),
+                    context,
+                    onUserImageClickedListener);
         } else {
-            return new ViewHolder(inflater.inflate(R.layout.fragment_news_item, parent, false));
+            return new NewsListViewHolder(
+                    inflater.inflate(R.layout.fragment_news_item, parent, false),
+                    context,
+                    onUserImageClickedListener);
         }
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull NewsListViewHolder holder, int position) {
         News news = newsList.get(position);
+        holder.setNewsID(new ObjectId(news.getNewsID()));
 
         DataRepository.getUser(new ObjectId(news.getNewsID()), userOwner -> {
+            holder.setUserOwnerID(new ObjectId(news.getNewsUserOwner()));
             holder.newsPostContent.setText(news.getNewsContent());
             holder.newsLikeCounter.setText(String.valueOf(news.getNewsLikes()));
            // holder.newsCommentCounter.setText(String.valueOf(news.getNewsComments().size()));
@@ -78,6 +74,7 @@ public class NewsListAdapter extends RecyclerView.Adapter<NewsListAdapter.ViewHo
             if(news.getNewsUserRecognized() != null){
                 holder.newsRecognitionLayout.setVisibility(View.VISIBLE);
                 DataRepository.getUser(new ObjectId(news.getNewsUserRecognized()), userRecognized -> {
+                    holder.setUserRecognizedID(new ObjectId(news.getNewsUserRecognized()));
                     holder.newsUsername.setText(UserNameGetter.getUserNameRecognition(userOwner, userRecognized));
 
                     if(userRecognized.getUserImage() != null)
@@ -130,158 +127,5 @@ public class NewsListAdapter extends RecyclerView.Adapter<NewsListAdapter.ViewHo
     @Override
     public int getItemCount() {
         return newsList.size();
-    }
-
-    public class ViewHolder extends RecyclerView.ViewHolder {
-        TextView         newsUsername;
-        ImageView        newsUserImage;
-        ImageView        newsPostImage;
-        TextView         newsPostContent;
-        ImageButton      newsLikeButton;
-        ImageButton      newsCommentButton;
-        ConstraintLayout newsPostLayout;
-        ConstraintLayout newsRecognitionLayout;
-        ConstraintLayout newsContainer;
-        ImageView        newsRecognitionUserImage;
-        TextView         newsLikeCounter;
-        GifImageView     newsStar;
-        TextView         newsTime;
-        TextView         newsCommentCounter;
-        private GestureDetector gestureDetector;
-
-        @SuppressLint("ClickableViewAccessibility")
-        ViewHolder(View itemView) {
-            super(itemView);
-
-            newsUsername             = itemView.findViewById(R.id.news_item_username);
-            newsPostContent          = itemView.findViewById(R.id.news_item_post_content);
-            newsUserImage            = itemView.findViewById(R.id.news_item_user_image);
-            newsPostImage            = itemView.findViewById(R.id.news_item_post_image);
-            newsLikeButton           = itemView.findViewById(R.id.news_item_like_button);
-            newsCommentButton        = itemView.findViewById(R.id.news_item_comment_button);
-            newsRecognitionLayout    = itemView.findViewById(R.id.news_item_recognition_layout);
-            newsRecognitionUserImage = itemView.findViewById(R.id.news_item_recognition_user_image);
-            newsPostLayout           = itemView.findViewById(R.id.news_item_post_layout);
-            newsContainer            = itemView.findViewById(R.id.news_item_container);
-            newsLikeCounter          = itemView.findViewById(R.id.news_item_like_counter);
-            newsCommentCounter       = itemView.findViewById(R.id.news_item_comment_counter);
-            newsStar                 = itemView.findViewById(R.id.news_item_star);
-            newsTime                 = itemView.findViewById(R.id.news_item_time);
-
-            gestureDetector = new GestureDetector(itemView.getContext(), new SingleTapConfirm());
-
-            starImageFunction();
-            commentButtonFunction();
-            containerFunction();
-            likeButtonFunction();
-            userImageFunction();
-            recognitionUserImageFunction();
-
-        }
-
-        private void starImageFunction(){
-            ((GifDrawable)newsStar.getDrawable()).stop();
-            newsStar.setOnClickListener(v -> {
-                ((GifDrawable)newsStar.getDrawable()).start();
-                ((GifDrawable)newsStar.getDrawable()).addAnimationListener(i -> {
-                    ((GifDrawable)newsStar.getDrawable()).stop();
-                });
-            });
-        }
-
-        @SuppressLint("ClickableViewAccessibility")
-        private void containerFunction(){
-            newsContainer.setOnTouchListener(new View.OnTouchListener() {
-                @Override
-                public boolean onTouch(View v, MotionEvent event) {
-                    gestureDetectorDouble.onTouchEvent(event);
-                    return false;
-                }
-
-                private final GestureDetector gestureDetectorDouble = new GestureDetector(context, new GestureDetector.SimpleOnGestureListener() {
-
-                    @SuppressLint("ClickableViewAccessibility")
-                    @Override
-                    public boolean onDoubleTap(MotionEvent e) {
-                        DataRepository.likeNews(new ObjectId(newsList.get(getAdapterPosition()).getNewsID()), new OnNewsLikedListener() {
-                            @Override
-                            public void liked(int likesCount) {
-                                newsLikeButton.setImageResource(R.drawable.ic_baseline_favorite_red_24);
-                            }
-
-                            @Override
-                            public void disliked(int likesCount) {
-                                if (Preferences.getDarkMode())
-                                    newsLikeButton.setImageResource(R.drawable.ic_baseline_favorite_border_white_24);
-                                else
-                                    newsLikeButton.setImageResource(R.drawable.ic_baseline_favorite_border_24);
-                            }
-                        });
-                        return super.onDoubleTap(e);
-                    }
-                });
-            });
-        }
-
-        private void userImageFunction(){
-            newsUserImage.setOnClickListener(v -> {
-                onUserImageClickedListener.onUserImageClicked(new ObjectId(newsList.get(getAdapterPosition()).getNewsUserOwner()));
-            });
-        }
-
-        private void recognitionUserImageFunction(){
-            newsRecognitionUserImage.setOnClickListener(v -> {
-                onUserImageClickedListener.onUserImageClicked(new ObjectId(newsList.get(getAdapterPosition()).getNewsUserRecognized()));
-            });
-        }
-
-        @SuppressLint("ClickableViewAccessibility")
-        private void likeButtonFunction(){
-            newsLikeButton.setOnTouchListener((v, event) -> {
-                if(gestureDetector.onTouchEvent(event)) {
-                    newsLikeButton.setAlpha((float) 1);
-                    DataRepository.likeNews(new ObjectId(newsList.get(getAdapterPosition()).getNewsID()), new OnNewsLikedListener() {
-                        @Override
-                        public void liked(int likesCount) {
-                            newsLikeButton.setImageResource(R.drawable.ic_baseline_favorite_red_24);
-                        }
-
-                        @Override
-                        public void disliked(int likesCount) {
-                            if (Preferences.getDarkMode())
-                                newsLikeButton.setImageResource(R.drawable.ic_baseline_favorite_border_white_24);
-                            else
-                                newsLikeButton.setImageResource(R.drawable.ic_baseline_favorite_border_24);
-                        }
-                    });
-
-                } else if(event.getAction() == MotionEvent.ACTION_DOWN){
-                    newsLikeButton.setAlpha((float) 0.5);
-                } else if (event.getAction() == MotionEvent.ACTION_UP){
-                    newsLikeButton.setAlpha((float) 1);
-                } else if (event.getAction() == MotionEvent.ACTION_CANCEL){
-                    newsLikeButton.setAlpha((float) 1);
-                }
-                return true;
-            });
-        }
-
-        @SuppressLint("ClickableViewAccessibility")
-        private void commentButtonFunction(){
-            newsCommentButton.setOnTouchListener((v, event) -> {
-                if(gestureDetector.onTouchEvent(event)) {
-                    newsCommentButton.setAlpha((float) 1);
-                    CommentDialog commentDialog = new CommentDialog(new ObjectId(newsList.get(getAdapterPosition()).getNewsID()));
-                    commentDialog.show(((MainActivity)context).getSupportFragmentManager(), commentDialog.getTag());
-                } else if(event.getAction() == MotionEvent.ACTION_DOWN){
-                    newsCommentButton.setAlpha((float) 0.5);
-                } else if (event.getAction() == MotionEvent.ACTION_UP){
-                    newsCommentButton.setAlpha((float) 1);
-                } else if (event.getAction() == MotionEvent.ACTION_CANCEL){
-                    newsCommentButton.setAlpha((float) 1);
-                }
-                return true;
-            });
-        }
     }
 }
---
+++ app/src/main/java/com/globapp/globapp/view/viewholders/NewsListViewHolder.java
@@ -0,0 +1,198 @@
+package com.globapp.globapp.view.viewholders;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.globapp.globapp.R;
+import com.globapp.globapp.data.DataRepository;
+import com.globapp.globapp.data.listeners.OnNewsLikedListener;
+import com.globapp.globapp.data.listeners.OnUserImageClickedListener;
+import com.globapp.globapp.data.local.Preferences;
+import com.globapp.globapp.util.SingleTapConfirm;
+import com.globapp.globapp.view.MainActivity;
+import com.globapp.globapp.view.dialogs.CommentDialog;
+
+import org.bson.types.ObjectId;
+
+import pl.droidsonroids.gif.GifDrawable;
+import pl.droidsonroids.gif.GifImageView;
+
+public class NewsListViewHolder extends RecyclerView.ViewHolder {
+    // UI COMPONENTS
+    public TextView         newsUsername;
+    public ImageView        newsUserImage;
+    public ImageView        newsPostImage;
+    public TextView         newsPostContent;
+    public ImageButton      newsLikeButton;
+    public ImageButton      newsCommentButton;
+    public ConstraintLayout newsPostLayout;
+    public ConstraintLayout newsRecognitionLayout;
+    public ConstraintLayout newsContainer;
+    public ImageView        newsRecognitionUserImage;
+    public TextView         newsLikeCounter;
+    public GifImageView     newsStar;
+    public TextView         newsTime;
+    public TextView         newsCommentCounter;
+
+    // data
+    private ObjectId newsID;
+    private ObjectId userOwnerID;
+    private ObjectId userRecognizedID;
+    private final GestureDetector gestureDetector;
+    private final Context   context;
+
+    // Listeners
+    private final OnUserImageClickedListener onUserImageClickedListener;
+
+    @SuppressLint("ClickableViewAccessibility")
+    public NewsListViewHolder(View itemView, Context context, OnUserImageClickedListener onUserImageClickedListener) {
+        super(itemView);
+        this.onUserImageClickedListener = onUserImageClickedListener;
+        this.context = context;
+
+        newsUsername             = itemView.findViewById(R.id.news_item_username);
+        newsPostContent          = itemView.findViewById(R.id.news_item_post_content);
+        newsUserImage            = itemView.findViewById(R.id.news_item_user_image);
+        newsPostImage            = itemView.findViewById(R.id.news_item_post_image);
+        newsLikeButton           = itemView.findViewById(R.id.news_item_like_button);
+        newsCommentButton        = itemView.findViewById(R.id.news_item_comment_button);
+        newsRecognitionLayout    = itemView.findViewById(R.id.news_item_recognition_layout);
+        newsRecognitionUserImage = itemView.findViewById(R.id.news_item_recognition_user_image);
+        newsPostLayout           = itemView.findViewById(R.id.news_item_post_layout);
+        newsContainer            = itemView.findViewById(R.id.news_item_container);
+        newsLikeCounter          = itemView.findViewById(R.id.news_item_like_counter);
+        newsCommentCounter       = itemView.findViewById(R.id.news_item_comment_counter);
+        newsStar                 = itemView.findViewById(R.id.news_item_star);
+        newsTime                 = itemView.findViewById(R.id.news_item_time);
+
+        gestureDetector = new GestureDetector(itemView.getContext(), new SingleTapConfirm());
+
+        starImageFunction();
+        commentButtonFunction();
+        containerFunction();
+        likeButtonFunction();
+        userImageFunction();
+        recognitionUserImageFunction();
+
+    }
+
+    private void starImageFunction(){
+        ((GifDrawable)newsStar.getDrawable()).stop();
+        newsStar.setOnClickListener(v -> {
+            ((GifDrawable)newsStar.getDrawable()).start();
+            ((GifDrawable)newsStar.getDrawable()).addAnimationListener(i -> ((GifDrawable)newsStar.getDrawable()).stop());
+        });
+    }
+
+    public void setUserOwnerID(ObjectId userOwnerID){
+        this.userOwnerID = userOwnerID;
+    }
+
+    public void setUserRecognizedID(ObjectId userRecognizedID){
+        this.userRecognizedID = userRecognizedID;
+    }
+
+    public void setNewsID(ObjectId newsID){
+        this.newsID = newsID;
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private void containerFunction(){
+        newsContainer.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                gestureDetectorDouble.onTouchEvent(event);
+                return false;
+            }
+
+            private final GestureDetector gestureDetectorDouble = new GestureDetector(context, new GestureDetector.SimpleOnGestureListener() {
+
+                @SuppressLint("ClickableViewAccessibility")
+                @Override
+                public boolean onDoubleTap(MotionEvent e) {
+                    DataRepository.likeNews(newsID, new OnNewsLikedListener() {
+                        @Override
+                        public void liked(int likesCount) {
+                            newsLikeButton.setImageResource(R.drawable.ic_baseline_favorite_red_24);
+                        }
+
+                        @Override
+                        public void disliked(int likesCount) {
+                            if (Preferences.getDarkMode())
+                                newsLikeButton.setImageResource(R.drawable.ic_baseline_favorite_border_white_24);
+                            else
+                                newsLikeButton.setImageResource(R.drawable.ic_baseline_favorite_border_24);
+                        }
+                    });
+                    return super.onDoubleTap(e);
+                }
+            });
+        });
+    }
+
+    private void userImageFunction(){
+        newsUserImage.setOnClickListener(v -> onUserImageClickedListener.onUserImageClicked(userOwnerID));
+    }
+
+    private void recognitionUserImageFunction(){
+        newsRecognitionUserImage.setOnClickListener(v -> onUserImageClickedListener.onUserImageClicked(userRecognizedID));
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private void likeButtonFunction(){
+        newsLikeButton.setOnTouchListener((v, event) -> {
+            if(gestureDetector.onTouchEvent(event)) {
+                newsLikeButton.setAlpha((float) 1);
+                DataRepository.likeNews(newsID, new OnNewsLikedListener() {
+                    @Override
+                    public void liked(int likesCount) {
+                        newsLikeButton.setImageResource(R.drawable.ic_baseline_favorite_red_24);
+                    }
+
+                    @Override
+                    public void disliked(int likesCount) {
+                        if (Preferences.getDarkMode())
+                            newsLikeButton.setImageResource(R.drawable.ic_baseline_favorite_border_white_24);
+                        else
+                            newsLikeButton.setImageResource(R.drawable.ic_baseline_favorite_border_24);
+                    }
+                });
+
+            } else if(event.getAction() == MotionEvent.ACTION_DOWN){
+                newsLikeButton.setAlpha((float) 0.5);
+            } else if (event.getAction() == MotionEvent.ACTION_UP){
+                newsLikeButton.setAlpha((float) 1);
+            } else if (event.getAction() == MotionEvent.ACTION_CANCEL){
+                newsLikeButton.setAlpha((float) 1);
+            }
+            return true;
+        });
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private void commentButtonFunction(){
+        newsCommentButton.setOnTouchListener((v, event) -> {
+            if(gestureDetector.onTouchEvent(event)) {
+                newsCommentButton.setAlpha((float) 1);
+                CommentDialog commentDialog = new CommentDialog(newsID);
+                commentDialog.show(((MainActivity)context).getSupportFragmentManager(), commentDialog.getTag());
+            } else if(event.getAction() == MotionEvent.ACTION_DOWN){
+                newsCommentButton.setAlpha((float) 0.5);
+            } else if (event.getAction() == MotionEvent.ACTION_UP){
+                newsCommentButton.setAlpha((float) 1);
+            } else if (event.getAction() == MotionEvent.ACTION_CANCEL){
+                newsCommentButton.setAlpha((float) 1);
+            }
+            return true;
+        });
+    }
+}
